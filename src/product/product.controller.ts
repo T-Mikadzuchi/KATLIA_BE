31654a12deb9626482324727aa3227e6ed5f0d09
Trang <thutrangpham1509@gmail.com>
2022-10-21 20:49:19 +0700

    delete yarn.lock, get product by gender

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(private productService: ProductService) {}
+  @Get('getProductByGender/:gender')
+  getProductByGender(@Param('gender') gender: string) {
+    gender = gender.toLowerCase();
+    return this.productService.getProductByGender(gender);
+  }
+}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,57 @@
+import { PrismaService } from './../prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(private prismaService: PrismaService) {}
+  async displayProductListByCategory(category: any, productList: any) {
+    const findProduct = await this.prismaService.product.findMany({
+      where: {
+        categoryId: category.categoryId,
+      },
+      select: {
+        productId: true,
+        name: true,
+        price: true,
+        discountId: true,
+      },
+    });
+    for (const product of findProduct) {
+      const productColor = await this.prismaService.product_detail.groupBy({
+        by: ['productId', 'colorId'],
+        where: {
+          productId: product.productId,
+        },
+      });
+      productList.push({
+        id: product.productId,
+        name: product.name,
+        colorNumber: productColor.length,
+        price: product.price,
+        discountId: product.discountId,
+      });
+    }
+  }
+
+  async getProductByGender(gender: string) {
+    const categoryList = await this.prismaService.product_category.findMany({
+      where: {
+        gender: gender
+      },
+      select: {
+        categoryId: true,
+      },
+    });
+    const productList: {
+      id: number;
+      name: string;
+      colorNumber: number;
+      price: number;
+      discountId: string;
+    }[] = [];
+    for (const category of categoryList) {
+      await this.displayProductListByCategory(category, productList)
+    }
+    return productList;
+  }
+}
