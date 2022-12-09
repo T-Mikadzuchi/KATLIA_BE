fcf879bef10a0de9dc3b4785c7a9327d7e9eceed
Trang <thutrangpham1509@gmail.com>
2022-12-09 22:34:42 +0700

    test up ava

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,17 @@
+import { AzureStorageModule } from '@nestjs/azure-storage';
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  imports: [
+    AzureStorageModule.withConfig({
+      sasKey: process.env['AZURE_STORAGE_SAS_KEY'],
+      accountName: process.env['AZURE_STORAGE_ACCOUNT'],
+      containerName: 'img',
+    }),
+  ],
 })
 export class UserModule {}
