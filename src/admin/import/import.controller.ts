320eaf5d46711b2713cdff608028347faa4fd12d
Trang <thutrangpham1509@gmail.com>
2022-12-16 23:44:15 +0700

    format

--- src/admin/import/import.controller.ts
+++ src/admin/import/import.controller.ts
@@ -1,4 +1,9 @@
-import { Controller, UseGuards, Get, Put, Post,Param,Body } from '@nestjs/common';
+import {
+  Controller,
+  UseGuards,
+  Put,
+  Param,
+} from '@nestjs/common';
 import { ImportService } from './import.service';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JwtGuard } from 'src/auth/guard/jwt.guard';
@@ -9,14 +14,13 @@ import { user } from '@prisma/client';
 @ApiTags('Admin Staff')
 @Controller('import')
 export class ImportController {
-    constructor( private importService: ImportService){    
-    }
-    @Put('confirmImport/:id')
-    confirmImport(@GetUser() user:user, @Param('id')  importId:string){
-        return this.importService.confirmImport(user,importId);
-    }
-    @Put('cancelImport/:id')
-    cancelImport(@GetUser() user: user,@Param('id') importId: string){
-        return this.importService.cancelImport(user,importId);
-    }
+  constructor(private importService: ImportService) {}
+  @Put('confirmImport/:id')
+  confirmImport(@GetUser() user: user, @Param('id') importId: string) {
+    return this.importService.confirmImport(user, importId);
+  }
+  @Put('cancelImport/:id')
+  cancelImport(@GetUser() user: user, @Param('id') importId: string) {
+    return this.importService.cancelImport(user, importId);
+  }
 }
--- src/admin/import/import.service.ts
+++ src/admin/import/import.service.ts
@@ -5,85 +5,83 @@ import { user } from '@prisma/client';
 
 @Injectable()
 export class ImportService {
-    constructor(private prismaService: PrismaService){}
-    async isPermission( user: user){
-        return (user.role=='ADMIN');
-    }
-    async confirmImport(user: user,importId: string){
-        if (!(await this.isPermission(user)))
-        throw new ForbiddenException('Permission denied');
-        const storageImport= await this.prismaService.storage_import.findUnique({
-            where:{
-                id: importId,
-            }
+  constructor(private prismaService: PrismaService) {}
+  async isPermission(user: user) {
+    return user.role == 'ADMIN';
+  }
+  async confirmImport(user: user, importId: string) {
+    if (!(await this.isPermission(user)))
+      throw new ForbiddenException('Permission denied');
+    const storageImport = await this.prismaService.storage_import.findUnique({
+      where: {
+        id: importId,
+      },
+    });
+    if (storageImport.status == 1) {
+      const confirmImport = await this.prismaService.storage_import.update({
+        where: {
+          id: importId,
+        },
+        data: {
+          status: 2,
+        },
+      });
+      const importList = await this.prismaService.import_detail.findMany({
+        where: {
+          importId: importId,
+        },
+        select: {
+          productId: true,
+          colorId: true,
+          size: true,
+          quantity: true,
+        },
+      });
+      for (const itemImport of importList) {
+        const item = await this.prismaService.product_detail.findFirst({
+          where: {
+            productId: itemImport.productId,
+            colorId: itemImport.colorId,
+            size: itemImport.size,
+          },
         });
-        if(storageImport.status==1){
-            const confirmImport= await this.prismaService.storage_import.update({
-                where:{
-                    id: importId,
-                }, data:{
-                    status:2,
-                }
-            });
-            const importList= await this.prismaService.import_detail.findMany({
-                where:{
-                    importId: importId,
-                }, select:{
-                    productId: true,
-                    colorId: true,
-                    size: true,
-                    quantity: true,
-                }
-            });
-            for(const itemImport of importList){
-                const item= await this.prismaService.product_detail.findFirst({
-                    where:{
-                        productId: itemImport.productId,
-                        colorId: itemImport.colorId,
-                        size: itemImport.size,
-                    }
-                });
-                let ex_quantity=item.quantity;
-                await this.prismaService.product_detail.updateMany({
-                    where:{
-                        productId: itemImport.productId,
-                        colorId: itemImport.colorId,
-                        size: itemImport.size,
-                    }, data:{
-                        quantity: ex_quantity +itemImport.quantity,
-                    }
-                });
-            }
-            return confirmImport;
-        }
-        else{
-            throw new ForbiddenException("Can't confirm this import!");
-        }
-        
-    }
-    async cancelImport(user:user, importId: string){
-        if (!(await this.isPermission(user)))
-        throw new ForbiddenException('Permission denied');
-        const storageImport= await this.prismaService.storage_import.findUnique({
-            where:{
-                id: importId,
-            }
+        const ex_quantity = item.quantity;
+        await this.prismaService.product_detail.updateMany({
+          where: {
+            productId: itemImport.productId,
+            colorId: itemImport.colorId,
+            size: itemImport.size,
+          },
+          data: {
+            quantity: ex_quantity + itemImport.quantity,
+          },
         });
-        if(storageImport.status==1){
-            const cancelImport= await this.prismaService.storage_import.update({
-                where:{
-                    id: importId,
-                }, data:{
-                    status: 3
-                }
-            });
-            return cancelImport;
-        }
-        else
-        {
-            throw new ForbiddenException("Can't cancel import");
-        }
-        
+      }
+      return confirmImport;
+    } else {
+      throw new ForbiddenException("Can't confirm this import!");
     }
-
+  }
+  async cancelImport(user: user, importId: string) {
+    if (!(await this.isPermission(user)))
+      throw new ForbiddenException('Permission denied');
+    const storageImport = await this.prismaService.storage_import.findUnique({
+      where: {
+        id: importId,
+      },
+    });
+    if (storageImport.status == 1) {
+      const cancelImport = await this.prismaService.storage_import.update({
+        where: {
+          id: importId,
+        },
+        data: {
+          status: 3,
+        },
+      });
+      return cancelImport;
+    } else {
+      throw new ForbiddenException("Can't cancel import");
+    }
+  }
 }
